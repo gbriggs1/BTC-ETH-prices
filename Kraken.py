003e33d81e03c45d91da91e3ca1f43ae3f041bbe
gbriggs1 <51299861+gbriggs1@users.noreply.github.com>
2021-11-02 19:03:41 -0400

    Update Kraken.py

--- Kraken.py
+++ Kraken.py
@@ -1,10 +1,20 @@
 import requests
 
-resp = requests.get('https://api.kraken.com/0/public/Spread?pair=BTCUSD')
+def BTC():
+    resp = requests.get('https://api.kraken.com/0/public/Spread?pair=BTCUSD')
 
-bid = resp.json()['result']['XXBTZUSD'][0][1]
-ask = resp.json()['result']['XXBTZUSD'][0][2]
-d = {}
-d['bid'] = bid
-d['ask'] = ask
-return d
+    bid = round(float(resp.json()['result']['XXBTZUSD'][0][1]), 3)
+    ask = round(float(resp.json()['result']['XXBTZUSD'][0][2]), 3)
+    d = {}
+    d['bid'] = "Buy: $"+str(bid)
+    d['ask'] = "Sell: $"+str(ask)
+    return d
+def ETH():
+    resp = requests.get('https://api.kraken.com/0/public/Spread?pair=ETHUSD')
+
+    bid = round(float(resp.json()['result']['XETHZUSD'][0][1]), 3)
+    ask = round(float(resp.json()['result']['XETHZUSD'][0][2]), 3)
+    d = {}
+    d['bid'] = "Buy: $"+str(bid)
+    d['ask'] = "Sell: $"+str(ask)
+    return d
