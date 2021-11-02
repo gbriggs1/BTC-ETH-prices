637c38872bf4577885008e2f9e6fa010016a70b4
gbriggs1 <51299861+gbriggs1@users.noreply.github.com>
2021-11-02 19:01:19 -0400

    Update Coinbase.py

--- Coinbase.py
+++ Coinbase.py
@@ -1,12 +1,19 @@
 import requests
 
-url = "https://api.exchange.coinbase.com/products/BTC-USD/ticker"
+def BTC():
+    url = "https://api.exchange.coinbase.com/products/BTC-USD/ticker"
+    headers = {"Accept": "application/json"}
+    response = requests.request("GET", url, headers=headers)
+    d = {}
+    d['bid'] = "Buy: $"+str(response.json()['bid'])
+    d['ask'] = "Sell: $"+str(response.json()['ask'])
+    return d
 
-headers = {"Accept": "application/json"}
-
-response = requests.request("GET", url, headers=headers)
-d = {}
-d['bid'] = response.json()['bid']
-d['ask'] = response.json()['ask']
-
-return d
+def ETH():
+    url = "https://api.exchange.coinbase.com/products/ETH-USD/ticker"
+    headers = {"Accept": "application/json"}
+    response = requests.request("GET", url, headers=headers)
+    d = {}
+    d['bid'] = "Buy: $"+str(response.json()['bid'])
+    d['ask'] = "Sell: $"+str(response.json()['ask'])
+    return d
